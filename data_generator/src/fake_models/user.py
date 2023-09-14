abdb6f9dc9a0db2cf65406234ecd18d16519f45b
xh4vm <h4vm@yandex.ru>
2023-09-14 07:03:07 -0400

    update: data gen

--- data_generator/src/fake_models/user.py
+++ data_generator/src/fake_models/user.py
@@ -6,3 +6,9 @@ from .base import fake
 class FakeUser(BaseModel):
     email: str = Field(default_factory=fake.email)
     password: str = Field(default_factory=fake.password)
+
+    def request_data(self):
+        return {
+            "email": self.email,
+            "password": self.password,
+        }
